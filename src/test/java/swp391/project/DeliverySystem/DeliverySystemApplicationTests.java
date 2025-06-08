d4568664b9931f909cf19136d46bef260a56dd20
Hoang Thi Van Anh <anhhtv2001@gmail.com>
2025-06-08 20:27:12 +0700

    SWP

---
+++ src/test/java/swp391/project/DeliverySystem/DeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391.project.DeliverySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
